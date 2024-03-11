1f094e8780f2886cb2a4b4486904e593c4f97f8e
shenjinqi <jinqishen@gmail.com>
2024-03-11 23:59:36 +0000

    demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,58 @@
 import streamlit as st
 import pandas as pd
+
+st.title("Shenjinqi's Streamlit App")
  
 st.write("""
 # My first app
 Hello *world!*
 """)
- 
-#df = pd.read_csv("my_data.csv")
-#st.line_chart(df)
+
+st.json({
+    'foo': 'bar',
+    'baz': 'boz',
+    'stuff': [
+        'stuff 1',
+        'stuff 2',
+        'stuff 3',
+        'stuff 5',
+    ],
+})
+
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
+import pandas as pd
+df = pd.DataFrame({'col1': [1,2,3]})
+df  # 👈 Draw the dataframe
+
+x = 10
+'x', x  # 👈 Draw the string 'x' and then the value of x
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+df = pd.read_csv(DATA_URL, nrows=5)
+st.line_chart(df)
